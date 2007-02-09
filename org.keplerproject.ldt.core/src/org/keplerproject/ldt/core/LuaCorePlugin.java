cab6f86560dfdacc0303749c328e9d7983d42321
guilherme <guilherme>
2007-02-09 01:24:34 +0000

    *** empty log message ***

--- org.keplerproject.ldt.core/src/org/keplerproject/ldt/core/LuaCorePlugin.java
+++ org.keplerproject.ldt.core/src/org/keplerproject/ldt/core/LuaCorePlugin.java
@@ -1,25 +1,25 @@
 /*
-* Copyright (C) 2003-2007 Kepler Project.
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * Copyright (C) 2003-2007 Kepler Project.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 package org.keplerproject.ldt.core;
 
@@ -30,9 +30,6 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Plugin;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.ImageRegistry;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -44,11 +41,9 @@ import org.osgi.framework.BundleContext;
  */
 public class LuaCorePlugin extends Plugin {
 
-	private static final String PLUGIN_CORE_ID = "org.keplerproject.ldt.core";
-
 	// The shared instance.
 	private static LuaCorePlugin plugin;
-	private ImageRegistry imgReg;
+
 
 	/**
 	 * The constructor.
@@ -62,11 +57,9 @@ public class LuaCorePlugin extends Plugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		imgReg = new ImageRegistry();
-		initializeImageRegistry(imgReg);
+
 	}
-	
-    
+
 	/**
 	 * This method is called when the plug-in is stopped
 	 */
@@ -82,80 +75,39 @@ public class LuaCorePlugin extends Plugin {
 		return plugin;
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path.
-	 * 
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_CORE_ID, path);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#initializeImageRegistry(org.eclipse.jface.resource.ImageRegistry)
-	 */
-	protected void initializeImageRegistry(ImageRegistry reg) {
-		String[] images = { "function", "table", "string", "userdata" };
-
-		for (int i = 0; i < images.length; i++) {
-			ImageDescriptor image = null;
-
-			image = LuaCorePlugin.getImageDescriptor("icons/" + images[i]
-					+ ".gif");
-			
-			reg.put(images[i],(ImageDescriptor) image);
+	public static IProject[] getLuaProjects() {
+		List rubyProjectsList = new ArrayList();
+		IProject workspaceProjects[] = ResourcesPlugin.getWorkspace().getRoot()
+				.getProjects();
+		for (int i = 0; i < workspaceProjects.length; i++) {
+			IProject iProject = workspaceProjects[i];
+			if (isLuaProject(iProject))
+				rubyProjectsList.add(iProject);
 		}
 
-		//super.initializeImageRegistry(reg);
+		IProject rubyProjects[] = new IProject[rubyProjectsList.size()];
+		return (IProject[]) rubyProjectsList.toArray(rubyProjects);
 	}
-	
-	 public static IProject[] getLuaProjects()
-	    {
-	        List rubyProjectsList = new ArrayList();
-	        IProject workspaceProjects[] = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-	        for(int i = 0; i < workspaceProjects.length; i++)
-	        {
-	            IProject iProject = workspaceProjects[i];
-	            if(isLuaProject(iProject))
-	                rubyProjectsList.add(iProject);
-	        }
 
-	        IProject rubyProjects[] = new IProject[rubyProjectsList.size()];
-	        return (IProject[])rubyProjectsList.toArray(rubyProjects);
-	    }
-
-	    public static LuaProject getLuaProject(String name)
-	    {
-	        IProject aProject = ResourcesPlugin.getWorkspace().getRoot().getProject(name);
-	        if(isLuaProject(aProject))
-	        {
-	            LuaProject theLuaProject = new LuaProject();
-	            theLuaProject.setProject(aProject);
-	            return theLuaProject;
-	        } else
-	        {
-	            return null;
-	        }
-	    }
-	    public static boolean isLuaProject(IProject aProject)
-	    {
-	        try
-	        {
-	            return aProject.hasNature("org.keplerproject.ldt.core.project.LuaProjectNature");
-	        }
-	        catch(CoreException coreexception)
-	        {
-	            return false;
-	        }
-	    }
-
-		public ImageRegistry getImageRegistry() {
-			return imgReg;
+	public static LuaProject getLuaProject(String name) {
+		IProject aProject = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject(name);
+		if (isLuaProject(aProject)) {
+			LuaProject theLuaProject = new LuaProject();
+			theLuaProject.setProject(aProject);
+			return theLuaProject;
+		} else {
+			return null;
 		}
+	}
+
+	public static boolean isLuaProject(IProject aProject) {
+		try {
+			return aProject
+					.hasNature("org.keplerproject.ldt.core.project.LuaProjectNature");
+		} catch (CoreException coreexception) {
+			return false;
+		}
+	}
 
 }
