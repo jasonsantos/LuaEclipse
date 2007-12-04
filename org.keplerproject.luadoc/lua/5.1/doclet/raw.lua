6e9115ef767d5a9095d54636759f1f85e5ab586d
jasonsantos <jasonsantos>
2007-12-04 05:51:03 +0000

    added plug-in for redistribution of LuaDoc 3.0.0

---
+++ org.keplerproject.luadoc/lua/5.1/doclet/raw.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------
+-- @release $Id$
+-----------------------------------------------------------------
+
+module "luadoc.doclet.raw"
+
+-----------------------------------------------------------------
+-- Generate the output.
+-- @param doc Table with the structured documentation.
+
+function start (doc)
+end
